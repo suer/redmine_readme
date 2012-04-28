26ca6563fb8b478399c268b421f19ad0423b104d
suer <suetsugu.r@gmail.com>
2012-04-28 11:11:12 +0900

    read README from multi-repositories*

--- lib/readme_listener.rb
+++ lib/readme_listener.rb
@@ -2,21 +2,22 @@ class ReadmeListener < Redmine::Hook::ViewListener
   MARKDOWN_EXTENSIONS = %w(.markdown .md .mkd .mdown .mkdn)
 
   def view_projects_show_left(context)
-    repository = context[:project].repository
-    return '' if repository.nil? or repository.entries.nil?
-    repository.fetch_changesets if Setting.autofetch_changesets?
-    entry = repository.entries.find{|e| e.name =~ /README((\.).*)?/i}
-    return '' if entry.nil?
-
-    text = repository.cat(entry.path)
-    formatter_name = '' # name for NullFormatter
-    if MARKDOWN_EXTENSIONS.include?(File.extname(entry.path))
-      formatter_name = Redmine::WikiFormatting.format_names.find {|name| name =~ /Markdown/}
-    end
-    formatter = Redmine::WikiFormatting.formatter_for(formatter_name).new(text)
-    context[:controller].send(:render_to_string, {
+    html = ''
+    context[:project].repositories.each do |repository|
+      next if repository.nil? or repository.entries.nil?
+      entry = repository.entries.find{|e| e.name =~ /README((\.).*)?/i}
+      next if entry.nil?
+      text = repository.cat(entry.path)
+      formatter_name = '' # name for NullFormatter
+      if MARKDOWN_EXTENSIONS.include?(File.extname(entry.path))
+        formatter_name = Redmine::WikiFormatting.format_names.find {|name| name =~ /Markdown/}
+      end
+      formatter = Redmine::WikiFormatting.formatter_for(formatter_name).new(text)
+      html << context[:controller].send(:render_to_string, {
         :partial => 'readme/left',
-        :locals => {:html => formatter.to_html}
+        :locals => {:repository => repository, :html => formatter.to_html}
       })
+    end
+    html
   end
 end
