9deaac9bda8bd3e085b935f680ffd8c83b211d6b
Ryo SUETSUGU <suetsugu.r@gmail.com>
2015-01-20 00:00:27 +0900

    Bumped: 0.0.2

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_readme do
   name 'Redmine Readme plugin'
   author 'suer'
   description 'display readme in project top pages'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/suer/redmine_readme'
   author_url 'http://d.hatena.ne.jp/suer/'
 end
